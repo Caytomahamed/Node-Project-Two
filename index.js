ab73c2d7d5e6fcce3177b549098911cbd35c496f
cayto <aidarous1143@gmail.com>
2022-04-11 21:33:28 +0300

    add PORT dynamic

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-
-//LOCAL ENVERONMENT
-
-//PRODUCTION ENVERIONMENT
-
-//
-
+require("dotenv").config(); 
+//must be at tobe
 
 // require your server and launch it
 //import server
 const server = require('./api/server.js');
 
-
 //DYNAMIC PORT HEROKUE ENVIRONMENT  => NPM I dotenv
-const port = 4000;
+
+const port = process.env.PORT;
+
+console.log(port);    
 
 server.listen(port, () => {
     console.log(`\n**** server Running on http://localhost:${port} ***\n`);
 })
 
+//LOCAL ENVERONMENT
+//PRODUCTION ENVERIONMENT
