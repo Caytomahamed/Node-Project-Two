87a9d42cc9a6cf4b02a807a2ba291e0c63e9e710
cayto <aidarous1143@gmail.com>
2022-04-11 21:03:08 +0300

    i dotenv make dynamic port on is herokue

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
-//local Enverionment
+//LOCAL ENVERONMENT
+
+//PRODUCTION ENVERIONMENT
+
+//
+
 
 // require your server and launch it
 //import server
 const server = require('./api/server.js');
 
+
+//DYNAMIC PORT HEROKUE ENVIRONMENT  => NPM I dotenv
 const port = 4000;
 
 server.listen(port, () => {
